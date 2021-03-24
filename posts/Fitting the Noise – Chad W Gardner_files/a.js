c811ef486287ab7dc35931513748fbb685b16014
Chad Gardner <cwgardner@protonmail.com>
2021-03-24 15:24:40 -0400

    2018 posts rebuilt

--- posts/Fitting the Noise – Chad W Gardner_files/a.js
+++
@@ -1,9 +0,0 @@
-/**/ WPCOMSharing.update_facebook_count({
-   "error": {
-      "message": "(#2) Service temporarily unavailable",
-      "type": "OAuthException",
-      "is_transient": true,
-      "code": 2,
-      "fbtrace_id": "ARp8gIX6OcCQGEZeUcRwXPh"
-   }
-});
